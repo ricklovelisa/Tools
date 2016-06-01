593f64213c374542a14a60d5159b2737ed2e6813
QQ <ricklovelisa@126.com>
2016-06-01 21:38:52 +0800

    update code

--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ scalaVersion := "2.10.4"
 
 resolvers += "Kunyan Repo" at "http://222.73.34.92:8081/nexus/content/groups/public/"
 
-libraryDependencies += "com.kunyan" % "nlpsuit-package" % "0.2.6.1"
+libraryDependencies += "com.kunyan" % "nlpsuit-package" % "0.2.6.4"
 
 libraryDependencies += "org.apache.spark" % "spark-mllib_2.10" % "1.5.2"
 
--- src/main/scala/tools/dataCleaning.scala
+++ src/main/scala/tools/dataCleaning.scala
@@ -1,12 +1,44 @@
 package tools
 
+import java.io.{File, FileInputStream, ObjectInputStream, Serializable}
+
 import org.apache.spark.{SparkConf, SparkContext}
 import java.net.{URLDecoder, URLEncoder}
+import java.util.regex.Pattern
+
+import com.kunyandata.nlpsuit.classification.Bayes
+import com.kunyandata.nlpsuit.sentiment.PredictWithNb
+import com.kunyandata.nlpsuit.util.{KunyanConf, TextPreprocessing}
+
+import scala.collection.Map
+import scala.collection.mutable.ArrayBuffer
 
 /**
   * Created by QQ on 2016/5/31.
   */
 object dataCleaning {
+
+  def change8To16(code: String): String = {
+
+    val url = code.split("%u")
+    val arr = ArrayBuffer[String]()
+
+    if (!url(0).isEmpty) {
+      arr += url(0)
+    }
+    for (i <- 1 until url.length) {
+
+      if (!url(i).isEmpty && url(i).length == 4) {
+        arr += ("%" + url(i).substring(0, 2))
+        arr += ("%" + url(i).substring(2))
+      }
+
+    }
+
+    val processedUrl = arr.mkString("")
+
+    processedUrl
+  }
 
   def main(args: Array[String]) {
 
@@ -15,24 +47,40 @@ object dataCleaning {
       .setMaster("local")
     val sc = new SparkContext(conf)
 
-    val data = sc.textFile("D:/14")
-//    val da = data.map(line => {
+    val kunyanConf = new KunyanConf
+    kunyanConf.set("222.73.57.17", 16003)
+
+    val patternUTF8 = Pattern.compile("%([0-9a-fA-F]){2}")
+    val patternUnicode = Pattern.compile("%u([0-9a-fA-F]){4}")
+//    sc.textFile("D:/14").coalesce(4).foreach(line => {
 //      val temp = line.split("\t")
-//      (temp(1), temp(7).split(" "))
-//    }).groupByKey().map(line => {
-//      val tmp = line._2.flatMap(_.toList)
-//      (line._1, tmp)
+////      val wordsTemp = temp(7).replaceAll("25", "")
+//      if (patternUnicode.matcher())
+//      while () {
+//
+//      }
+//      println(temp(1), wordsTemp)
+//      if (patternUTF8.matcher(wordsTemp).find)
+//        (temp(1), URLDecoder.decode(wordsTemp, "UTF-8"))
+//      else if (patternUnicode.matcher(wordsTemp).find)
+//        (temp(1), URLDecoder.decode(change8To16(wordsTemp), "UTF-16"))
+//      else
+//      (temp(1), wordsTemp)
+//
 //    })
 
-    val kk = data.map(line => {
-      val temp = line.split("\t")
-      temp(7).split(",")
-    }).flatMap(_.toList)
 
-    kk.filter(line => line.count(_ == '%') > 1).foreach(println)
-    println(URLDecoder.decode("%25u660E%25u56ED%25u68EE%25u6797%25u90FD%25u5E02".replace("25",""), "Bytes"))
+    val k = "fadfa%25u6587%25u6021%25u82B1%25u56ED%2520%25u4E09%25u5C45"
+    println(k.substring(5,8))
+    var i = 1
+    val xx = patternUTF8.matcher(k)
+    while (xx.find()) {
+      println(xx.start())
+      println(xx.end())
+      println(xx.group())
+      println()
+      val result = URLDecoder.decode(xx.group(), "UTF-8")
+    }
 
-
-    println("%25u660E%25u56ED%25u68EE%25u6797%25u90FD%25u5E02".getBytes("Unicode").toSeq)
   }
 }
