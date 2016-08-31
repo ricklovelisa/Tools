e64d4d1eab1371c9dac15af662164ae2d54bf556
QiuQiu <ricklovelisa@126.com>
2016-08-31 18:13:08 +0800

    update code

---
+++ src/main/scala/tools/parser/WriteOpcodeToMysql.scala
@@ -0,0 +1,14 @@
+package tools.parser
+
+import tools.util.MySQLUtils
+
+/**
+  * Created by QQ on 8/31/16.
+  */
+object WriteOpcodeToMysql {
+
+  def main(args: Array[String]) {
+
+    val conn = MySQLUtils.getConnect()
+  }
+}
